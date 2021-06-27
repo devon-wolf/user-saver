abf36178a147abf8d4a58d2c15d6ac0e3229f1b9
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-27 10:32:15 -0700

    Clarify type

--- src/services/getGitHubUser.ts
+++ src/services/getGitHubUser.ts
@@ -1,13 +1,14 @@
 import fetch from 'node-fetch';
+import { GHUser } from '../types';
 
 const URL = 'https://api.github.com/users';
 
 // TODO improve error handling here
-export const getGitHubUser  = async (user : string) : Promise<any> => {
+export const getGitHubUser  = async (user : string) : Promise<GHUser | number> => {
 	const response = await fetch(`${URL}/${user}`);
 
 	if (response.status === 200) {
-		const json = await response.json();
+		const json : GHUser = await response.json();
 		return json;
 	}
 
