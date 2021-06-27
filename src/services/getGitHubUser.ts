c391afb19842c5b95ad84fa39ce141bf7042f2ca
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-27 10:26:15 -0700

    Add return type to fetch function

--- src/services/getGitHubUser.ts
+++ src/services/getGitHubUser.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const URL = 'https://api.github.com/users';
 
 // TODO improve error handling here
-export const getGitHubUser  = async (user : string) => {
+export const getGitHubUser  = async (user : string) : Promise<any> => {
 	const response = await fetch(`${URL}/${user}`);
 
 	if (response.status === 200) {
