0c22375492a80b2fdf67dc9928bf94ee931364f8
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-27 11:45:38 -0700

    Move firebase logic into hook, try to add teardown logic

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { User } from '../types';
-import { usersRef } from '../services/firebaseUtils';
+import { useFirebase } from './useFirebase';
 
 // TODO figure out if it should make a fresh GH fetch each time the display is populated, this data can get stale fast as-is; consider just storing a list of usernames and making fetches regularly?
 
@@ -8,44 +8,10 @@ const useUsers = () : {
 	users: User[],
 	setUsers: React.Dispatch<React.SetStateAction<User[]>>
 } => {
+	
 	const [users, setUsers] = useState<User[]>([]);
 
-	useEffect(() => {
-		usersRef.on('value', snapshot => {
-			const snapshotUsers = snapshot.val();
-			const newState = [];
-			
-			// TODO determine if this looping is as inefficient as I suspect it is
-			for (const user in snapshotUsers) {
-				const {
-					id,
-					url,
-					login,
-					name,
-					public_repos,
-					public_gists,
-					followers,
-					following,
-					created_at
-				} = snapshotUsers[user];
-
-				newState.push({
-					id,
-					url,
-					login,
-					name,
-					public_repos,
-					public_gists,
-					followers,
-					following,
-					created_at
-				});
-			}
-
-			setUsers(newState);
-		});
-		
-	}, []);
+	useFirebase({ setUsers });
 
 	return {
 		users,
