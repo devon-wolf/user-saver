e4762897f7c20e9c6c44e1cad3a1c8cb2ab0bc2c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-28 18:53:17 -0700

    Clean up getGitHubUser function

--- src/services/getGitHubUser.ts
+++ src/services/getGitHubUser.ts
@@ -3,16 +3,11 @@ import { GHUser } from '../types';
 
 const URL = 'https://api.github.com/users';
 
-// TODO improve error handling here
-export const getGitHubUser  = async (user : string) : Promise<GHUser | number> => {
-	const response = await fetch(`${URL}/${user}`);
-
-	if (response.status === 200) {
-		const json : GHUser = await response.json();
-		return json;
-	}
-
-	else {
-		return response.status;
-	}
+export const getGitHubUser  = (user : string) : Promise<GHUser | number> => {
+	return fetch(`${URL}/${user}`)
+		.then(response =>
+			response.ok
+				? response.json()
+				: response.status)
+		.catch(console.error);
 };
